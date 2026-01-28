4fa3649fb8cebc1a2d17103007540c342081d85d
Hypnohippy <131181867+Hypnohippy@users.noreply.github.com>
2026-01-28 10:10:39 +0000

    Update page.tsx

--- app/oauth/facebook/pick-page/page.tsx
+++ app/oauth/facebook/pick-page/page.tsx
@@ -1,16 +1,5 @@
-// app/oauth/facebook/pick-page/page.tsx
 import PickFacebookPageClient from "./pick-facebook-page-client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function PickFacebookPagePage({
-  searchParams,
-}: {
-  searchParams: Record<string, string | string[] | undefined>;
-}) {
-  const token = typeof searchParams.token === "string" ? searchParams.token : "";
-  const state = typeof searchParams.state === "string" ? searchParams.state : "";
-
-  return <PickFacebookPageClient token={token} state={state} />;
+export default function PickFacebookPagePage() {
+  return <PickFacebookPageClient />;
 }
