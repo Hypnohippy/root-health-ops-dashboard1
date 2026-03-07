e5b84509353b401927c1d96369be9224432c50f2
Hypnohippy <131181867+Hypnohippy@users.noreply.github.com>
2026-03-07 07:22:34 +0000

    Update redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,20 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/tiktok-developers-site-verification.txt",
+        source: "/tiktok-developers-site-verification.txt/",
         destination: "/tiktok-developers-site-verification.txt",
         permanent: false,
       },
       {
-        source: "/tiktok-developers-site-verification.txt/",
-        destination: "/tiktok-developers-site-verification.txt",
-        permanent: false,
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "roothealthops.com",
+          },
+        ],
+        destination: "https://www.roothealthops.com/:path*",
+        permanent: true,
       },
     ];
   },
